1fa461ce506acd61bb3048d09d7c9288c65c00cd
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2025-11-20 22:02:56 +0530

    added searching order

--- components/SearchOrder.tsx
+++ components/SearchOrder.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 function SearchOrder() {
   const [orderId, setOrderId] = useState("");
+  const router = useRouter();
 
   const handleSubmit = (evnt: React.FormEvent) => {
     evnt.preventDefault();
@@ -11,6 +13,8 @@ function SearchOrder() {
     if (!orderId) return;
 
     setOrderId("");
+
+    router.push(`/order/${orderId}`);
   };
 
   return (
@@ -18,7 +22,7 @@ function SearchOrder() {
       <input
         type="text"
         placeholder="Search order #"
-        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:ring focus:ring-yellow-500 focus:ring-offset-2 focus:outline-none sm:w-64 sm:focus:w-72"
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-offset-2 sm:w-64 sm:focus:w-72"
         value={orderId}
         onChange={(evnt) => setOrderId(evnt.target.value)}
       />
